280d275cac53273c8ff514cd38a7abb56886491b
Mina <minakamali.d@gmail.com>
2023-02-20 08:24:43 +0330

    Footer & base tag addded

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Products from "./components/Products/Products";
 import "./App.css";
 import Title from "./components/Title/Title";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <div>
       <Title />
       <Products />;
+      <Footer />
     </div>
   );
 };
